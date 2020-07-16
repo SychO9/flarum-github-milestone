bee5bed24d6d0a7c3a732740ad930fd38de5a2c4
SychO9 <sychocouldy@gmail.com>
2020-07-16 12:28:40 +0100

    Initialize repository

---
+++ extend.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @package Flarum GitHub Milestone Activity
+ * @author Sami "SychO" Mazouz (https://github.com/SychO9)
+ * @version 0.1.0
+ * @license MIT
+ */
+
+return [
+    //...
+];
