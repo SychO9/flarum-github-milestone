03b3950b77c68ae2f817b4f9085f9f0159df9c68
SychO9 <sychocouldy@gmail.com>
2020-08-16 23:01:10 +0100

    Add milestone route

--- extend.php
+++ extend.php
@@ -12,7 +12,8 @@ use Flarum\Extend;
 return [
     (new Extend\Frontend('forum'))
         ->js(__DIR__.'/js/dist/forum.js')
-        ->css(__DIR__.'/less/forum.less'),
+        ->css(__DIR__.'/less/forum.less')
+        ->route('/milestone', 'milestone'),
 
     (new Extend\Frontend('admin'))
         ->js(__DIR__.'/js/dist/admin.js'),
